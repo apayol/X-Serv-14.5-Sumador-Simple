ad025e9436e5f92ecf4669dee8b1fa48558e8aa7
apayol <apayolmontero@gmail.com>
2018-02-13 23:20:29 +0100

    Corregido normas estilo

--- servidor-sumador.py
+++ servidor-sumador.py
@@ -17,17 +17,17 @@ try:
         http_recibido = str(recvSocket.recv(2048), 'utf-8')
         relativa = http_recibido.split()[1]    # /op1/función/op2
         print(relativa)
-        _, op1, funcion, op2 = relativa.split('/')    #guardo cada término
+        _, op1, funcion, op2 = relativa.split('/')    # guardo cada término
 
         try:
             op1 = float(op1)
             op2 = float(op2)
             resultado = calculadora.funciones[funcion](op1, op2)
         except KeyError:
-            resultado = "FAIL! Funciones aceptadas: sumar, restar, multiplicar, dividir o exp"
+            resultado = "FAIL! Funcion no aceptada"
         except ValueError:
             resultado = "FAIL! Los operandos han de ser numeros"
-        
+
         # Respuesta:
         print('Answering back...\n')
         html_respuesta = "<html><body><h2>Calculadora &uarr; </h2>"
@@ -35,7 +35,8 @@ try:
         html_respuesta += str(resultado)
         html_respuesta += "</h4></p></body></html>"
 
-        recvSocket.send(bytes("HTTP/1.1 200 OK\r\n\r\n" + html_respuesta + "\r\n", 'utf-8'))
+        recvSocket.send(bytes(
+            "HTTP/1.1 200 OK\r\n\r\n" + html_respuesta + "\r\n", 'utf-8'))
         recvSocket.close()
 
 except KeyboardInterrupt:
